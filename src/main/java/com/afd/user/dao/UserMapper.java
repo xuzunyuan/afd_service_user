3bf484a9a475dff745b84cb81a93c52294ec500a
liubo <liubo@yiwang.com>
2015-01-12 11:20:11 +0800

    a

--- src/main/java/com/afd/user/dao/UserMapper.java
+++ src/main/java/com/afd/user/dao/UserMapper.java
@@ -1,5 +1,7 @@
 package com.afd.user.dao;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.afd.model.user.User;
 
 public interface UserMapper {
@@ -14,4 +16,7 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    @Select("SELECT count(*) FROM t_user WHERE user_name = #{userName}")
+	int userCount(String userName);
 }
---
+++ src/main/java/com/afd/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.afd.user.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.afd.service.user.IUserService;
+import com.afd.user.dao.UserMapper;
+
+public class UserServiceImpl implements IUserService {
+
+	@Autowired
+	private UserMapper userMapper;
+	@Override
+	public boolean uniqueUserName(String userName) {
+		int count = this.userMapper.userCount(userName);
+		return false;
+	}
+
+}
