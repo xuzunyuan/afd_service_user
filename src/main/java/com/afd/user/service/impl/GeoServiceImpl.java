0367665807fdfd4b4ec4e36df16102ced89106c2
liubo <liubo@yiwang.com>
2015-01-13 11:14:14 +0800

    v

---
+++ src/main/java/com/afd/user/service/impl/GeoServiceImpl.java
@@ -0,0 +1,16 @@
+package com.afd.user.service.impl;
+
+import java.util.List;
+
+import com.afd.model.user.Geo;
+import com.afd.service.user.IGeoService;
+
+public class GeoServiceImpl implements IGeoService {
+
+	@Override
+	public List<Geo> getGeoLevelOne() {
+		
+		return null;
+	}
+
+}
