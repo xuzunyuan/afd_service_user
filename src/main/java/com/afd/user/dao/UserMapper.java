e7199252fc54938b869b6084f9047f25722eeb85
liubo <liubo@yiwang.com>
2015-01-16 12:05:08 +0800

    v

--- src/main/java/com/afd/user/dao/UserMapper.java
+++ src/main/java/com/afd/user/dao/UserMapper.java
@@ -1,5 +1,6 @@
 package com.afd.user.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.afd.model.user.User;
@@ -19,4 +20,6 @@ public interface UserMapper {
 
     @Select("SELECT count(*) FROM t_user WHERE user_name = #{userName}")
 	int userCount(String userName);
+    
+    User selectUserByUserName(@Param("userName")String userName);
 }
--- src/main/java/com/afd/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/afd/user/service/impl/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package com.afd.user.service.impl;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.afd.common.util.DateUtils;
+import com.afd.constants.SystemConstants;
 import com.afd.model.user.User;
 import com.afd.service.user.IUserService;
 import com.afd.user.dao.UserMapper;
 
+@Service("userService")
 public class UserServiceImpl implements IUserService {
 
 	@Autowired
@@ -21,5 +28,42 @@ public class UserServiceImpl implements IUserService {
 	@Override
 	public User getUserById(Long userId){
 		return this.userMapper.selectByPrimaryKey(userId);
+	}
+	@Override
+	public Long register(User user) {
+		if(user!=null){
+			//注册日期
+			user.setRegDate(DateUtils.currentDate());
+			String userKey = RandomStringUtils.randomAlphanumeric(20);
+			String pwd = DigestUtils.md5Hex(DigestUtils.md5Hex(userKey+SystemConstants.WEB_KEY)+user.getPwd());
+			user.setPwdKey(userKey);
+			user.setPwd(pwd);
+			this.userMapper.insertSelective(user);
+			return user.getUserId();
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(RandomStringUtils.randomAlphanumeric(20));
+	}
+	@Override
+	public boolean logIn(String userName, String pwd) {
+		if(StringUtils.isBlank(userName) || StringUtils.isBlank(pwd)){
+			return false;
+		}
+		
+		User user = this.userMapper.selectUserByUserName(userName);
+		if(user == null){
+			return false;
+		}
+		
+		String userKey = user.getPwdKey();
+		String validPwd = DigestUtils.md5Hex(DigestUtils.md5Hex(userKey+SystemConstants.WEB_KEY)+pwd);
+		if(validPwd.equals(user.getPwd())){
+			return true;
+		}
+		
+		return false;
 	}
 }
