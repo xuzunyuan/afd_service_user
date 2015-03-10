1c471ab737062bbf6b77c80c2123b40ed4f220c3
xuzunyuan <xuzunyuan@yiwang.com>
2015-03-10 19:40:56 +0800

    v

--- src/main/java/com/afd/user/dao/UserMapper.java
+++ src/main/java/com/afd/user/dao/UserMapper.java
@@ -1,32 +1,39 @@
 package com.afd.user.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import com.afd.common.mybatis.Page;
 import com.afd.model.user.User;
+import com.afd.service.user.IUserService.UserQueryCondition;
 
 public interface UserMapper {
-    int deleteByPrimaryKey(Integer userId);
+	int deleteByPrimaryKey(Integer userId);
 
-    int insert(User record);
+	int insert(User record);
 
-    int insertSelective(User record);
+	int insertSelective(User record);
 
-    User selectByPrimaryKey(Long userId);
+	User selectByPrimaryKey(Long userId);
 
-    int updateByPrimaryKeySelective(User record);
+	int updateByPrimaryKeySelective(User record);
 
-    int updateByPrimaryKey(User record);
+	int updateByPrimaryKey(User record);
 
-    @Select("SELECT count(*) FROM t_user WHERE user_name = #{userName}")
+	@Select("SELECT count(*) FROM t_user WHERE user_name = #{userName}")
 	int userCount(String userName);
-    
-    User selectUserByUserName(@Param("userName")String userName);
 
-    @Select("SELECT count(*) FROM t_user WHERE mobile = #{mobile}")
+	User selectUserByUserName(@Param("userName") String userName);
+
+	@Select("SELECT count(*) FROM t_user WHERE mobile = #{mobile}")
 	int mobileCount(String mobile);
 
-	User selectUserByMobile(@Param("mobile")String mobile);
+	User selectUserByMobile(@Param("mobile") String mobile);
 
-	User getUserInfoById(@Param("userId")long userId);
+	User getUserInfoById(@Param("userId") long userId);
+
+	List<User> queryUserByPage(@Param("cond") UserQueryCondition cond,
+			@Param("page") Page<User> page);
 }
--- src/main/java/com/afd/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/afd/user/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.afd.common.mybatis.Page;
 import com.afd.common.util.DateUtils;
 import com.afd.constants.SystemConstants;
 import com.afd.model.user.User;
@@ -20,26 +21,30 @@ public class UserServiceImpl implements IUserService {
 	private UserMapper userMapper;
 	@Autowired
 	private UserExtMapper userExtMapper;
-	
+
 	@Override
 	public boolean uniqueUserName(String userName) {
 		int count = this.userMapper.userCount(userName);
-		if(count >0){
+		if (count > 0) {
 			return false;
 		}
 		return true;
 	}
+
 	@Override
-	public User getUserById(Long userId){
+	public User getUserById(Long userId) {
 		return this.userMapper.selectByPrimaryKey(userId);
 	}
+
 	@Override
 	public Long register(User user) {
-		if(user!=null){
-			//注册日期
+		if (user != null) {
+			// 注册日期
 			user.setRegDate(DateUtils.currentDate());
 			String userKey = RandomStringUtils.randomAlphanumeric(20);
-			String pwd = DigestUtils.md5Hex(DigestUtils.md5Hex(userKey+SystemConstants.WEB_KEY)+user.getPwd());
+			String pwd = DigestUtils.md5Hex(DigestUtils.md5Hex(userKey
+					+ SystemConstants.WEB_KEY)
+					+ user.getPwd());
 			user.setPwdKey(userKey);
 			user.setPwd(pwd);
 			this.userMapper.insertSelective(user);
@@ -47,59 +52,83 @@ public class UserServiceImpl implements IUserService {
 		}
 		return null;
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println(RandomStringUtils.randomAlphanumeric(20));
 	}
-	
+
 	@Override
 	public User getUserByUserName(String userName) {
 		return this.userMapper.selectUserByUserName(userName);
 	}
+
 	@Override
 	public int updateUser(User user) {
-		if(user!=null&&user.getUserId()!=null){
+		if (user != null && user.getUserId() != null) {
 			return this.userMapper.updateByPrimaryKeySelective(user);
 		}
 		return 0;
 	}
+
 	@Override
 	public boolean uniqueMobile(String mobile) {
 		int count = this.userMapper.mobileCount(mobile);
-		if(count >0){
+		if (count > 0) {
 			return false;
 		}
 		return true;
 	}
+
 	@Override
 	public User getUserByMobile(String mobile) {
 		return this.userMapper.selectUserByMobile(mobile);
 	}
+
 	@Override
 	public int chgPwd(String mobile, String pwd) {
 		User user = this.getUserByMobile(mobile);
-		if(user !=null){
+		if (user != null) {
 			String pwdKey = user.getPwdKey();
-			pwd = DigestUtils.md5Hex(DigestUtils.md5Hex(pwdKey+SystemConstants.WEB_KEY)+pwd);
+			pwd = DigestUtils.md5Hex(DigestUtils.md5Hex(pwdKey
+					+ SystemConstants.WEB_KEY)
+					+ pwd);
 			user.setPwd(pwd);
 			return this.updateUser(user);
 		}
 		return 0;
 	}
+
 	@Override
 	public User getUserInfoById(long userId) {
 		return this.userMapper.getUserInfoById(userId);
 	}
+
 	@Override
 	public int updateUserExt(UserExt userExt) {
-		if(userExt!=null&&userExt.getUserId()!=null){
-			UserExt ueTemp = this.userExtMapper.selectByPrimaryKey(userExt.getUserId());
-			if(ueTemp!=null){
+		if (userExt != null && userExt.getUserId() != null) {
+			UserExt ueTemp = this.userExtMapper.selectByPrimaryKey(userExt
+					.getUserId());
+			if (ueTemp != null) {
 				this.userExtMapper.updateByPrimaryKeySelective(userExt);
-			}else{
+			} else {
 				this.userExtMapper.insertSelective(userExt);
 			}
 		}
 		return 0;
+	}
+
+	@Override
+	public Page<User> queryUserByPage(UserQueryCondition cond, int... page) {
+		Page<User> pg = new Page<User>();
+
+		if (page != null && page.length > 0) {
+			pg.setCurrentPageNo(page[0]);
+			if (page.length > 1)
+				pg.setPageSize(page[1]);
+		}
+
+		pg.setResult(userMapper.queryUserByPage(cond, pg));
+
+		return pg;
 	}
 }
